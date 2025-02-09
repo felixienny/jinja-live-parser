185f74309510605cd68915cfa5bd35e56215b63d
Felix IENNY <fienny@hexanet.fr>
2025-02-09 10:21:22 +0100

    import and export tabs

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function
-
-from flask import Flask, render_template, request, make_response, redirect, url_for
-from jinja2 import Environment, meta, exceptions
+import json
+import io
+import base64
 from random import choice
 from html import escape
-import json
 import yaml
 from yaml import Loader
-import base64
-
+from flask import Flask, render_template, request, make_response, redirect, url_for, send_file
+from jinja2 import Environment, exceptions
 
 app = Flask(__name__)
 
@@ -17,8 +14,13 @@ app = Flask(__name__)
 def home():
     encoded_custom_tabs = request.cookies.get("custom_tabs")
     print(encoded_custom_tabs)
-    custom_tabs = json.loads(base64.b64decode(encoded_custom_tabs).decode('utf-8')) if encoded_custom_tabs else []
-    return render_template('index.html', tabs=custom_tabs)
+    custom_tabs = json.loads(base64.b64decode(encoded_custom_tabs).decode(
+        'utf-8')) if encoded_custom_tabs else []
+    # Set the cookie again each time the user visits the website
+    encoded_custom_tabs = base64.b64encode(json.dumps(custom_tabs).encode()).decode('utf-8')
+    response = make_response(render_template('index.html', tabs=custom_tabs))
+    response.set_cookie("custom_tabs", encoded_custom_tabs, max_age=3*365*24*60*60)  # 3 years in seconds
+    return response
 
 
 @app.route('/convert', methods=['GET', 'POST'])
@@ -56,6 +58,7 @@ def convert():
 
     return escape(rendered_jinja2_tpl).replace('\n', '<br />')
 
+
 @app.route('/save', methods=['POST'])
 def save():
     if request.method == 'POST':
@@ -70,7 +73,8 @@ def save():
 
         # Charger les onglets existants depuis les cookies
         encoded_custom_tabs = request.cookies.get("custom_tabs")
-        custom_tabs = json.loads(base64.b64decode(encoded_custom_tabs).decode()) if encoded_custom_tabs else []
+        custom_tabs = json.loads(base64.b64decode(
+            encoded_custom_tabs).decode()) if encoded_custom_tabs else []
 
         if tab_id:
             # Mettre à jour l'onglet
@@ -91,13 +95,15 @@ def save():
             })
 
         # Sauvegarder les onglets dans les cookies
-        encoded_custom_tabs = base64.b64encode(json.dumps(custom_tabs).encode()).decode('utf-8')
+        encoded_custom_tabs = base64.b64encode(
+            json.dumps(custom_tabs).encode()).decode('utf-8')
         response = make_response(json.dumps({"tab_id": len(custom_tabs) - 1}))
         response.set_cookie("custom_tabs", encoded_custom_tabs)
 
         return response
 
     return render_template('add_tab.html')
+
 
 @app.route('/get-tab', methods=['POST'])
 def get():
@@ -108,7 +114,8 @@ def get():
 
     # Charger les onglets depuis les cookies
     encoded_custom_tabs = request.cookies.get("custom_tabs")
-    custom_tabs = json.loads(base64.b64decode(encoded_custom_tabs).decode()) if encoded_custom_tabs else []
+    custom_tabs = json.loads(base64.b64decode(
+        encoded_custom_tabs).decode()) if encoded_custom_tabs else []
 
     try:
         tab = custom_tabs[int(tab_id)]
@@ -116,6 +123,7 @@ def get():
         return "Tab not found", 404
 
     return json.dumps(tab)
+
 
 @app.route('/del-tab', methods=['POST'])
 def delete():
@@ -126,7 +134,8 @@ def delete():
 
     # Charger les onglets depuis les cookies
     encoded_custom_tabs = request.cookies.get("custom_tabs")
-    custom_tabs = json.loads(base64.b64decode(encoded_custom_tabs).decode()) if encoded_custom_tabs else []
+    custom_tabs = json.loads(base64.b64decode(
+        encoded_custom_tabs).decode()) if encoded_custom_tabs else []
 
     # Supprimer l'onglet
     try:
@@ -135,8 +144,62 @@ def delete():
         return "Tab not found", 404
 
     # Sauvegarder les onglets dans les cookies
-    encoded_custom_tabs = base64.b64encode(json.dumps(custom_tabs).encode()).decode('utf-8')
+    encoded_custom_tabs = base64.b64encode(
+        json.dumps(custom_tabs).encode()).decode('utf-8')
     response = make_response(redirect(url_for('home')))
     response.set_cookie("custom_tabs", encoded_custom_tabs)
 
     return response
+
+
+@app.route('/random', methods=['GET'])
+def random():
+    return choice(["Hello", "World", "Foo", "Bar", "Baz"])
+
+
+@app.route('/export', methods=['POST'])
+def export_tabs():
+    # Charger les onglets depuis les cookies
+    encoded_custom_tabs = request.cookies.get("custom_tabs")
+    custom_tabs = json.loads(base64.b64decode(
+        encoded_custom_tabs).decode()) if encoded_custom_tabs else []
+    return send_file(
+        io.BytesIO(json.dumps(custom_tabs, indent=4).encode('utf-8')),
+        as_attachment=True,
+        mimetype='application/json',
+        download_name='jinja-live-parser-export-tabs.json'
+    )
+
+@app.route('/import', methods=['POST'])
+def import_tabs():
+    if request.method == 'POST':
+        # Charger les onglets depuis les cookies
+        encoded_custom_tabs = request.cookies.get("custom_tabs")
+        custom_tabs = json.loads(base64.b64decode(
+            encoded_custom_tabs).decode()) if encoded_custom_tabs else []
+
+        # Charger les onglets dans le fichier JSON
+        if 'file' not in request.files:
+            return "No file uploaded", 400
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return "No selected file", 400
+
+        try:
+            custom_tabs += json.loads(request.files['file'].read().decode())
+        except ValueError as e:
+            return "Value error in JSON: {0}".format(e)
+        except Exception as e:
+            return "Error: {0}".format(e)
+
+        # Sauvegarder les onglets dans les cookies
+        encoded_custom_tabs = base64.b64encode(
+            json.dumps(custom_tabs).encode()).decode('utf-8')
+        response = make_response(redirect(url_for('home')))
+        response.set_cookie("custom_tabs", encoded_custom_tabs)
+
+        return response
+
+    return render_template('index.html')
